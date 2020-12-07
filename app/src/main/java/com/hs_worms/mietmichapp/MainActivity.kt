8daef9ace2572c388552cb9077f2f20f19b20dd6
Abel Nguendia Tapondjou <inf3417@hs-worms.de>
2020-12-07 10:14:47 +0100

    Initial commit

---
+++ app/src/main/java/com/hs_worms/mietmichapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hs_worms.mietmichapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        //  A way to hide the status bar
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
